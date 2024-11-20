1c47edee4cf03d7d6136120f8b030a4421b33813
Julieverrs <165794975+Julieverrs@users.noreply.github.com>
2024-11-20 18:22:10 +0800

    update code

--- jobpostings/ml_model.py
+++ jobpostings/ml_model.py
@@ -1,16 +1,13 @@
 import pandas as pd
 
 def recommend_applicants(job_data):
-    # Load the updated dataset
+
     df = pd.read_csv('datasetss.csv')
 
-    # Print columns to check for any issues (debugging step)
     print(df.columns)
 
-    # Example: Matching applicants based on job required skills
     required_skills = job_data.get('required_skills', '')
-    
-    # Ensure the 'Skills' column exists (case-sensitive)
+
     if 'Skills' in df.columns:
         # Filter applicants based on matching required skills
         recommended = df[df['Skills'].str.contains(required_skills, na=False, case=False)]
